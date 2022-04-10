a9417b0286d9ec55997fa59026ffbfc067aa0d98
1-blue <1-blue98@naver.com>
2022-04-10 18:06:38 +0900

    first commit!

---
+++ README.md
@@ -0,0 +1,2 @@
+## 🚀 프로젝트 마무리하고 작성함 🚀
++ [trello](https://trello.com/b/T98vIu6a/capstonedesign-2022)
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <main>
+      <h1>Hello, capstone-design-2022</h1>
+    </main>
+  );
+};
+
+export default Home;
